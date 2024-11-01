a84786644afaa53cbde3200e10fcce22cf16f5bb
NilsonAlcides-1 <nilson.alcides@outlook.com>
2024-10-31 22:03:41 -0300

    add atributos

--- AppSaresp_2024/Models/Aluno.cs
+++ AppSaresp_2024/Models/Aluno.cs
@@ -3,6 +3,12 @@
     public class Aluno
     {
         public int Id { get; set; }
-        
+
+        public Stream Nome { get; set; }
+        public Stream Email { get; set; }
+        public Stream Telefone { get; set; }
+        public Stream Serie { get; set; }
+        public Stream Turma { get; set; }
+        public DateTime DaNasc { get; set; }
     }
 }
